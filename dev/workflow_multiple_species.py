18ddc1498a51eafe840e1b7648d201485660a977
Sylvain Haupert <sylvain.haupert@mnhn.fr>
2023-03-08 14:19:14 +0100

    DEV: add scripts for dev

--- dev/workflow_multiple_species.py
+++ dev/workflow_multiple_species.py
@@ -7,11 +7,6 @@ Authors : Felix Michaud and Sylvain Haupert
 
 """
 
-from IPython import get_ipython
-print(__doc__)
-# Clear all the variables
-get_ipython().magic('reset -sf')
-
 import shutil
 import numpy as np
 from pathlib import Path
@@ -42,9 +37,9 @@ SCIENTIC_NAME_LIST = [
                         # "Phoenicurus phoenicurus", 
                         # "Strix aluco", 
                         # "Aegithalos caudatus",
-                      ]
+                    ]
 
-CONFIG_FILE = '../config_default.yaml' 
+CONFIG_FILE = '../src/bambird/config_default.yaml' 
 
 # After the process, remove the audio that were saved during the process ?
 CLEAN = False
